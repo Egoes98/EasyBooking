0bfef094cb783543ae7b5b574a6c1ecc3de6922f
Egoitz <egoes98@gmail.com>
2019-01-15 17:30:35 +0100

    Cambio GUI

--- EasyBooking_Client/src/GUI/Login.java
+++ EasyBooking_Client/src/GUI/Login.java
@@ -52,34 +52,6 @@ public class Login {
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Facebook");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				//Conectar con Facebook
-				boolean correct = false;
-				try {
-					System.out.println(textField.getText());
-					//Parametros de conecxion
-					String email = textField.getText();
-					String password = "" + textField_1.getText();
-					correct = controller.loginUser(email,password);
-				} catch (RemoteException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				if(correct) {
-					new Booking(controller);
-					new Booked(controller);
-					frame.dispose();
-				}else {
-					System.out.println("Incorrect Account!!");
-				}
-			}
-		});
-		btnNewButton.setBounds(109, 157, 89, 23);
-		frame.getContentPane().add(btnNewButton);
-		
 		JButton btnNewButton_1 = new JButton("Register");
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
 			@Override
@@ -104,7 +76,11 @@ public class Login {
 		lblPassword.setBounds(135, 90, 64, 14);
 		frame.getContentPane().add(lblPassword);
 		
-		JButton btnNewButton_2 = new JButton("Google+");
+		JButton btnNewButton_2 = new JButton("Login");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		btnNewButton_2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -129,7 +105,7 @@ public class Login {
 				}
 			}
 		});
-		btnNewButton_2.setBounds(219, 157, 89, 23);
+		btnNewButton_2.setBounds(167, 155, 89, 23);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JLabel lblAuthorizationMethod = new JLabel("Authorization method");
